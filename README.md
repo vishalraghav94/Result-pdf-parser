2a6f6458a13dbe91153d738a9c9a460b63a0e572
Vishal Raghav <hemvishalraghav94@gmail.com>
2018-04-17 04:17:28 +0530

    Completed data extraction

---
+++ README.md
@@ -0,0 +1,11 @@
+#### Prerequisite:
+1. Install poppler
+	Mac - brew install poppler
+	why - This tool used pdfseparator which comes bundled with poppler
+2. run 'npm install'
+
+
+#### Usage
+	use command as:
+	node test.js pdf_filename outfilename.json
+	e.g. 'node test.js result.pdf 3rdsem.json'
--- test.js
+++ test.js
@@ -1,26 +1,41 @@
 var pdfreader = require('pdfreader');
-
+var fs = require('fs');
+var child_process = require('child_process');
+var file = process.argv[2];
+var dest_file = process.argv[3];
+console.log(file);
+console.log('pdfseparate ' + file + ' pdf_pages/%d.pdf');
+child_process.exec('mkdir pdf_pages');
+child_process.exec('pdfseparate ' + file + ' pdf_pages/%d.pdf',function
+      (error, stdout, stderr) {
+      if (error) {
+         console.log(error.stack);
+         console.log('Error code: '+error.code);
+         console.log('Signal received: '+error.signal);
+      }
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+   });
+var megaFinalArray = [];
 var rows = {}; // indexed by y-position
 var newArr = [];
-var filename = 'Sample.pdf';
+var filename = 'pdf_pages/';
 function printRows() {
+  newArr = [];
   Object.keys(rows) // => array of y-positions (type: float)
     .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
     .forEach(function(y) {
       newArr.push((rows[y] || []).join(','));
-      console.log((rows[y] || []).join(','));
     });
-    console.log(newArr);
     newArr.splice(0, 14);
     newArr.splice(newArr.length - 4, 4);
-    console.log(newArr);
     var finalArray;
     if (newArr.length > 1) {
       newArr = getFormattedArray(newArr);
       finalArray = getMarksObject(newArr);
     }
     console.log(finalArray);
-
+    megaFinalArray.push(finalArray);
 }
 function getMarksObject(arr) {
   var i, obj = {}, finalArray = [];
@@ -34,7 +49,6 @@ function parseMarksString(marksString) {
   marksString.split(',').map(function(ele) {
     return parseInt(ele.substring(0,indexOf('(')));
   })
-  console.log(marksString);
   var totalMarks = 0, i;
   for (i = 0; i < marksString.length; i++) {
     totalMarks += marksString[i];
@@ -62,9 +76,8 @@ function getFormattedObj(arr) {
 function parseMarksString(marksString) {
   marksString = marksString.split(',');
   marksString = marksString.map(function(ele) {
-    return parseInt(ele.substring(0,ele.indexOf('(')));
+    return parseInt(ele.substring(0,ele.indexOf('('))) || 0;
   });
-  console.log(marksString);
   var totalMarks = 0, i;
   for (i = 0; i < marksString.length; i++) {
     totalMarks += marksString[i];
@@ -81,15 +94,57 @@ function getFormattedArray(arr) {
   return newArr;
 }
 
-new pdfreader.PdfReader().parseFileItems(filename, function(err, item){
-  if (!item || item.page) {
-    // end of file, or page
-    printRows();
-    console.log('PAGE:', item.page);
-    rows = {}; // clear rows for next page
+var k = 1;
+var interval = setInterval(function() {
+  console.log(filename + k + '.pdf');
+  new pdfreader.PdfReader().parseFileItems(filename + k + '.pdf', function(err, item){
+    if (!item || item.page) {
+      // end of file, or page
+      printRows();
+      rows = {}; // clear rows for next page
+    }
+    else if (item.text) {
+      // accumulate text items into rows object, per line
+      (rows[item.y] = rows[item.y] || []).push(item.text);
+    }
+  });
+  k++;
+  if (k >=150) {
+    clearInterval(interval);
   }
-  else if (item.text) {
-    // accumulate text items into rows object, per line
-    (rows[item.y] = rows[item.y] || []).push(item.text);
+}, 300);
+function validObject(obj) {
+  if (obj.marks == 0) {
+    obj.marks = 1;
   }
-});
+  if (!obj.name || !obj.marks || !obj.enrol) {
+    return false;
+  }
+  if (!parseInt(obj.enrol)) {
+      return false;
+  }
+  if (obj.name.indexOf(',') !== -1) {
+    return false;
+  }
+  if (obj.marks == 1) {
+    obj.marks = 0;
+  }
+  return true;
+}
+
+setTimeout(function() {
+  megaFinalArray = megaFinalArray.filter((e) => e && e.length);
+  var i;
+  for (i = 0; i < megaFinalArray.length; i++) {
+    megaFinalArray[i] = megaFinalArray[i].filter((e) => validObject(e));
+  }
+  megaFinalArray = megaFinalArray.filter((e) => e && e.length);
+  fs.writeFile("./" + dest_file, JSON.stringify(megaFinalArray, null, 4), function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("The file was saved!");
+  });
+  child_process.exec('rm -rf pdf_pages/');
+}, 50000);
