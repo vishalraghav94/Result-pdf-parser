029a726e1b25fbd55e05a0c59c6e8da0214c49e2
Vishal Raghav <vishalraghav@Vishals-MacBook-Pro-2.local>
2023-05-19 19:51:29 +0530

    removed logs

--- server.js
+++ server.js
@@ -6,26 +6,25 @@ var busboy = require('connect-busboy');
 var child_process = require('child_process');
 const request = require('superagent');
 app.use(busboy());
-app.post('/upload', function(req, res) {
-    console.log(req.body);
+app.post('/upload', function (req, res) {
     var outputJsonFile = '';
     if (req.busboy) {
 
         req.pipe(req.busboy);
         var outputFile = [];
-        req.busboy.on('field', function(key, value, keyTruncated, valueTruncated) {
+        req.busboy.on('field', function (key, value, keyTruncated, valueTruncated) {
             outputJsonFile += value + '_';
         });
-        req.busboy.on('file', function(fieldname, file, filename) {
+        req.busboy.on('file', function (fieldname, file, filename) {
             var fstream = fs.createWriteStream('./' + filename);
-            outputJsonFile = outputJsonFile.substr(0,outputJsonFile.lastIndexOf('_')) + '.json';
+            outputJsonFile = outputJsonFile.substr(0, outputJsonFile.lastIndexOf('_')) + '.json';
             file.pipe(fstream);
-            fstream.on('close', function(err) {
+            fstream.on('close', function (err) {
                 if (err) {
-                    res.send({error: err});
+                    res.send({ error: err });
                 }
                 child_process.exec('node test.js ' + filename + ' ' + outputJsonFile);
-                res.send({data: 'file Uploaded'});
+                res.send({ data: 'file Uploaded' });
             })
         });
 
@@ -39,33 +38,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var overall;
 var marksObj;
-console.log(marksObj);
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + 'public/index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 var lastBranch;
 var globalEnrol, localRanks, globalRanks, lastSem, lastInstitute, collegeRanks, overallCollegeRanks, overallGlobalRanks, overallLocalRanks;
 let data;
 let count = 1;
 data = [];
-while(count <= 8) {
+while (count <= 8) {
     try {
         let json = fs.readFileSync('./jsondata/' + 'IT' + '_' + count++ + '.json');
         data.push(JSON.parse(json));
-    } catch(e) {
+    } catch (e) {
         console.error(e);
     }
 }
 makeOverallMarksObject('IT', data);
-app.get('/marks', function(req, res) {
+app.get('/marks', function (req, res) {
     let enrol = req.query.enrol;
     let sem = req.query.sem || 5;
     sem = parseInt(sem);
-    let branch = req.query.branch === "undefined" ?  'IT' : req.query.branch;
+    let branch = req.query.branch === "undefined" ? 'IT' : req.query.branch;
     try {
         overall = fs.readFileSync('jsondata/overall_' + branch + '.json');
         overall = JSON.parse(overall);
-    } catch(e) {
+    } catch (e) {
         makeOverallMarksObject(branch, data);
         setTimeout(function () {
             overall = fs.readFileSync('jsondata/overall_' + branch + '.json');
@@ -75,7 +73,7 @@ app.get('/marks', function(req, res) {
 
     try {
         if (sem >= count) {
-            res.send({error: 'This sem\'s result doesnt exist.'});
+            res.send({ error: 'This sem\'s result doesnt exist.' });
         }
         marksObj = Object.assign({}, data[sem - 1]);
         let currentEnrol = enrol.substr(enrol.length - 8, enrol.length - 1);
@@ -117,36 +115,36 @@ app.get('/marks', function(req, res) {
         newMarksObj.collegeRank = collegeRank + 1;
         newMarksObj.allSemMarks = allSemMarks;
         newMarksObj.overallRanks = overallRanks;
-        res.send({data: newMarksObj});
-    } catch(e) {
-        res.send({error: 'No data'});
+        res.send({ data: newMarksObj });
+    } catch (e) {
+        res.send({ error: 'No data' });
     }
 });
 
-app.get('/ranks', function(req, res) {
-  let enrolOffset = req.query.enrol;
-  let sem = req.query.sem || 5;
-  let branch = req.query.branch || 'IT';
-  let count = 1;
-  let data = [];
-    while(1) {
+app.get('/ranks', function (req, res) {
+    let enrolOffset = req.query.enrol;
+    let sem = req.query.sem || 5;
+    let branch = req.query.branch || 'IT';
+    let count = 1;
+    let data = [];
+    while (1) {
         try {
             let json = fs.readFileSync('./jsondata/' + branch + '_' + count++ + '.json');
             data.push(JSON.parse(json));
-        } catch(e) {
+        } catch (e) {
             break;
         }
     }
-  if (sem >= count) {
-      res.send({error: 'This sem\'s result doesnt exist.'});
-  }
-  marksObj = data[sem - 1];
-  let marks = marksObj[enrolOffset];
-  localRanks = getLocalRanks(marks);
-  res.send({data: localRanks});
+    if (sem >= count) {
+        res.send({ error: 'This sem\'s result doesnt exist.' });
+    }
+    marksObj = data[sem - 1];
+    let marks = marksObj[enrolOffset];
+    localRanks = getLocalRanks(marks);
+    res.send({ data: localRanks });
 });
 
-app.post('/helloworld', function(req, res) {
+app.post('/helloworld', function (req, res) {
     console.log("req accepted from slack.");
     let payload = req.body.payload ? JSON.parse(req.body.payload) : undefined;
 
@@ -175,24 +173,24 @@ app.post('/helloworld', function(req, res) {
     var authToken = 'xoxp-28632775939-380675780304-382632678855-12041307c7c9ac9f3050946a0f186659';
     if (payload && payload.callback_id === 'hello_world') {
         request.post('https://slack.com/api/dialog.open').set('Accept', 'application/json').set('Authorization', 'Bearer ' + authToken).send(body).end((err, res) => {
-                if (err) {
-                    console.log("Error occured", err);
-                }
-                else {
-                    console.log("response is ::", res);
-                }
+            if (err) {
+                console.log("Error occured", err);
+            }
+            else {
+                console.log("response is ::", res);
+            }
         });
     }
 
-   res.send(body);
+    res.send(body);
 });
 
-app.get('/globalRanks', function(req, res) {
+app.get('/globalRanks', function (req, res) {
     let branch = req.query.branch || 'IT';
     try {
         overall = fs.readFileSync('jsondata/overall_' + branch + '.json');
         overall = JSON.parse(overall);
-    } catch(e) {
+    } catch (e) {
         makeOverallMarksObject(branch, data);
         setTimeout(function () {
             overall = fs.readFileSync('jsondata/overall_' + branch + '.json');
@@ -200,41 +198,41 @@ app.get('/globalRanks', function(req, res) {
         }, 1000);
     }
     if (!overall) {
-        res.send({error: 'Mo data'});
+        res.send({ error: 'Mo data' });
     }
     overallGlobalRanks = overallGlobalRanks || getGlobalRanks(overall, true);
-    res.send({data: overallGlobalRanks});
+    res.send({ data: overallGlobalRanks });
 });
 
 
 
 
 function getLocalRanks(obj, isOverallRanks) {
-  var arr = [];
-  Object.keys(obj).forEach((e) => {
-    arr.push(obj[e]);
-  });
-  if (isOverallRanks) {
-      arr = arr.sort(function(obj1, obj2) {
-          return obj2.averageMarks - obj1.averageMarks;
-      });
-  }
-  else {
-      arr = arr.sort(function(obj1, obj2) {
-          return obj2.totalMarks - obj1.totalMarks;
-      });
-  }
-  return arr;
+    var arr = [];
+    Object.keys(obj).forEach((e) => {
+        arr.push(obj[e]);
+    });
+    if (isOverallRanks) {
+        arr = arr.sort(function (obj1, obj2) {
+            return obj2.averageMarks - obj1.averageMarks;
+        });
+    }
+    else {
+        arr = arr.sort(function (obj1, obj2) {
+            return obj2.totalMarks - obj1.totalMarks;
+        });
+    }
+    return arr;
 }
-app.get('/globalrank', function(req, res) {
-   let sem = req.query.sem;
-   let obj = data[sem - 1];
-   let arr = getGlobalRanks(obj);
-   res.send({data: arr});
+app.get('/globalrank', function (req, res) {
+    let sem = req.query.sem;
+    let obj = data[sem - 1];
+    let arr = getGlobalRanks(obj);
+    res.send({ data: arr });
 });
 function getGlobalRanks(obj, isOverallRank) {
     let marksArr = [];
-    Object.keys(obj).forEach((e) =>{
+    Object.keys(obj).forEach((e) => {
         Object.keys(obj[e]).forEach((f) => {
             marksArr.push(obj[e][f]);
         })
@@ -254,24 +252,23 @@ function getGlobalRanks(obj, isOverallRank) {
 function makeOverallMarksObject(branch, data) {
     console.log('overall:');
     if (!data || !data.length) {
-        return ;
+        return;
     }
     var obj = Object.assign({}, data[data.length - 1]);
     Object.keys(obj).forEach((e) => {
-       Object.keys(obj[e]).forEach(f => {
-           let allSemMarks = getOverallMarks(f);
-           allSemMarks = allSemMarks.filter(e => e > 0);
-          obj[e][f].allSemMarks = allSemMarks;
-          let averageMarks = 0;
-           allSemMarks.forEach(e => {
-               averageMarks += e;
-           });
-           obj[e][f].averageMarks = Math.round((averageMarks / allSemMarks.length) * 100) / 100
-       });
+        Object.keys(obj[e]).forEach(f => {
+            let allSemMarks = getOverallMarks(f);
+            allSemMarks = allSemMarks.filter(e => e > 0);
+            obj[e][f].allSemMarks = allSemMarks;
+            let averageMarks = 0;
+            allSemMarks.forEach(e => {
+                averageMarks += e;
+            });
+            obj[e][f].averageMarks = Math.round((averageMarks / allSemMarks.length) * 100) / 100
+        });
     });
-    console.log(obj);
-    fs.writeFile("./jsondata/" + 'overall_' + branch + '.json', JSON.stringify(obj, null, 4), function(err) {
-        if(err) {
+    fs.writeFile("./jsondata/" + 'overall_' + branch + '.json', JSON.stringify(obj, null, 4), function (err) {
+        if (err) {
             return console.log(err);
         }
 
@@ -282,11 +279,9 @@ function makeOverallMarksObject(branch, data) {
 
 function getOverallMarks(enrol) {
     let s;
-    console.log('getOverall: ' + enrol);
     let enrolOffset = enrol.substr(enrol.length - 8, enrol.length - 1);
     let allSemMarks = [];
     for (s = 0; s < data.length; s++) {
-        console.log('sem: ' + s);
         allSemMarks.push(data[s][enrolOffset] ? (data[s][enrolOffset][enrol] ? data[s][enrolOffset][enrol]['percent'] : 0) : 0);
     }
     return allSemMarks;
@@ -294,7 +289,7 @@ function getOverallMarks(enrol) {
 
 function getCollegeRank(obj, institute, isOverallRank) {
     let newArr = [];
-    Object.keys(obj).forEach((e) =>{
+    Object.keys(obj).forEach((e) => {
         Object.keys(obj[e]).forEach((f) => {
             if (obj[e][f].institute === institute)
                 newArr.push(obj[e][f]);
@@ -310,7 +305,7 @@ function getCollegeRank(obj, institute, isOverallRank) {
     return newArr;
 }
 
-Array.prototype.indexOfObj = function(enrol) {
+Array.prototype.indexOfObj = function (enrol) {
     var self = this;
     var index = -1;
     for (let i = 0; i < this.length; i++) {
@@ -323,19 +318,17 @@ Array.prototype.indexOfObj = function(enrol) {
 };
 
 function convertToJson(filename, reqBody, res) {
-  var outputJsonFile = reqBody.branch + '_' + reqBody.sem + '.json';
-  filename = './uploads/' + filename;
-  console.log(outputJsonFile);
-  console.log('node test.js ' + filename + ' ' + outputJsonFile);
-  child_process.exec('node test.js ' + filename + ' ' + outputJsonFile, function(err) {
-      if (err) {
-          res.send({error: 'File not uploaded'})
-      }
-      res.send({Success: 'file uploaded'});
-      makeOverallMarksObject(reqBody.branch, data);
-  });
+    var outputJsonFile = reqBody.branch + '_' + reqBody.sem + '.json';
+    filename = './uploads/' + filename;
+    child_process.exec('node test.js ' + filename + ' ' + outputJsonFile, function (err) {
+        if (err) {
+            res.send({ error: 'File not uploaded' })
+        }
+        res.send({ Success: 'file uploaded' });
+        makeOverallMarksObject(reqBody.branch, data);
+    });
 }
 
-app.listen(process.env.PORT || 4000, function() {
-  console.log('Server up and running at http://localhost:4000');
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Server up and running at http://localhost:4000');
 });
